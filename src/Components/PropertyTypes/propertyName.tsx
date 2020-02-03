1006bd6f28e09fe01c375869c27ffb563ceadec1
Xuleos <Evancat77@gmail.com>
2020-02-03 17:51:22 -0500

    property name text color fixed

--- src/Components/PropertyTypes/propertyName.tsx
+++ src/Components/PropertyTypes/propertyName.tsx
@@ -10,7 +10,7 @@ export default class PropertyName extends Roact.PureComponent<PropertyNameProps>
 				Size={new UDim2(0, 100, 0, this.props.Height)}
 				Text="Property"
 				TextScaled={true}
-				TextColor3={Color3.fromRGB(255, 255, 255)}
+				TextColor3={Color3.fromRGB(185, 185, 185)}
 				BackgroundTransparency={1}
 				TextXAlignment={Enum.TextXAlignment.Left}
 			>
