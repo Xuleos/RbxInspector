219d1a1836e45d9af9eccffa021114de419302b2
Xuleos <Evancat77@gmail.com>
2020-02-03 19:07:22 -0500

    fixed weird scaling issue

--- src/Components/PropertyTypes/stringType.tsx
+++ src/Components/PropertyTypes/stringType.tsx
@@ -6,6 +6,6 @@ interface StringTypeProps {
 }
 export default class StringType extends Roact.PureComponent<StringTypeProps> {
 	render() {
-		return <RoundedFrame SliceScale={0.5} Size={new UDim2(1, 0, 0, this.props.Height)}></RoundedFrame>;
+		return <RoundedFrame SliceScale={0.5} Size={new UDim2(0.7, 0, 0, this.props.Height)}></RoundedFrame>;
 	}
 }
